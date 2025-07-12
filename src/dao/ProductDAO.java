06700cf6769bdd6ae166bf33270fae0485186dfb
amandhakad13 <ad5538694@gmail.com>
2025-07-12 09:16:34 -0700

    search and delete method added

--- src/dao/ProductDAO.java
+++ src/dao/ProductDAO.java
@@ -11,7 +11,7 @@ import connection.DBConnection;
 import entity.Product;
 
 public class ProductDAO {
-	
+
 	public int insert(Product p) throws SQLException {
 		Connection con = DBConnection.getConnection();
 		String query = "INSERT INTO products VALUES (?,?,?,?,?,?)";
@@ -25,27 +25,57 @@ public class ProductDAO {
 		int isInsert = pstmt.executeUpdate();
 		return isInsert;
 	}
-	
+
 	public List<Product> display() throws SQLException {
 		Connection con = DBConnection.getConnection();
 		String query = "SELECT * FROM products";
 		PreparedStatement pstmt = con.prepareStatement(query);
 		ResultSet rs = pstmt.executeQuery();
 		List<Product> pl = new ArrayList<>();
-		while(rs.next()) {
+		while (rs.next()) {
 			Product p = new Product();
-			
+
 			p.setId(rs.getInt(1));
 			p.setName(rs.getString(2));
 			p.setPrice(rs.getFloat(3));
 			p.setQuantity(rs.getInt(4));
 			p.setManuDate(rs.getString(5));
 			p.setDiscount(rs.getFloat(6));
-			
+
 			pl.add(p);
 			p = null;
 		}
 		return pl;
 	}
-	
+
+	public int delete(int id) throws SQLException {
+		Connection con = DBConnection.getConnection();
+		String query = "DELETE FROM products where p_id = ?";
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setInt(1, id);
+		int isDelete = pstmt.executeUpdate();
+		return isDelete;
+	}
+
+	public Product search(int id) throws SQLException {
+		Connection con = DBConnection.getConnection();
+		String query = "select * from products where p_id = ?";
+		PreparedStatement pstmt = con.prepareStatement(query);
+		pstmt.setInt(1, id);
+		ResultSet rs = pstmt.executeQuery();
+		Product p = new Product();
+		if (rs.next()) {
+			p.setId(rs.getInt(1));
+			p.setName(rs.getString(2));
+			p.setPrice(rs.getFloat(3));
+			p.setQuantity(rs.getInt(4));
+			p.setManuDate(rs.getString(5));
+			p.setDiscount(rs.getFloat(6));
+		} else {
+			return null;
+		}
+		return p;
+
+	}
+
 }
--- src/test/ProductTest1.java
+++ src/test/ProductTest1.java
@@ -14,78 +14,129 @@ public class ProductTest1 {
 		Scanner sc = new Scanner(System.in);
 		ProductDAO pdao = new ProductDAO();
 		
-		System.out.println("-----------Product Menu-----------");
-		System.out.println("1. Insert Record");
-		System.out.println("2. Delete Record");
-		System.out.println("3. Display All Record");
-		System.out.println("4. Search Record");
-		System.out.println("5. Search Employee By name");
-		System.out.println("6. Exit The Application");
-		System.out.println();
-		
 		int choice = 0;
-		System.out.print("Enter Your Choice : ");
-		choice = sc.nextInt();
 		
-		switch(choice) {
-			case 1 :
-				System.out.print("Enter id : ");
-				int id = sc.nextInt();
-				System.out.print("Enter name : ");
-				String name = sc.next();
-				System.out.print("Enter price : ");
-				float price = sc.nextFloat();
-				System.out.print("Enter quantity : ");
-				int quantity = sc.nextInt();
-				System.out.print("Enter manufacture date : ");
-				String manu_date = sc.next();
-				System.out.print("Enter discount : ");
-				float discount = sc.nextFloat();
-				
-				Product p = new Product(id, name, price, quantity, manu_date, discount);
-				int isInsert=0;
-				try {
-					isInsert = pdao.insert(p);
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				
-				if(isInsert>0)
-					System.out.println("Record Stored Successfully");
-				else 
-					System.out.println("Something Went Wrong");
-				break;
-				
-			case 2 :
-				break;
-				
-			case 3 :
-				List<Product> pl = null;
-				try {
-					pl = pdao.display();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				
-				for(Product ps : pl) {
-					System.out.println(ps);
+		
+		do {
+			System.out.println("-----------Product Menu-----------");
+			System.out.println("1. Insert Record");
+			System.out.println("2. Delete Record");
+			System.out.println("3. Display All Record");
+			System.out.println("4. Search Record");
+			System.out.println("6. Exit The Application");
+			System.out.println();
+			System.out.print("Enter Your Choice : ");
+			choice = sc.nextInt();
+		
+			switch(choice) {
+				case 1 : {
+					System.out.print("Enter id : ");
+					int id = sc.nextInt();
+					System.out.print("Enter name : ");
+					String name = sc.next();
+					System.out.print("Enter price : ");
+					float price = sc.nextFloat();
+					System.out.print("Enter quantity : ");
+					int quantity = sc.nextInt();
+					System.out.print("Enter manufacture date : ");
+					String manu_date = sc.next();
+					System.out.print("Enter discount : ");
+					float discount = sc.nextFloat();
+					
+					Product p = new Product(id, name, price, quantity, manu_date, discount);
+					int isInsert=0;
+					try {
+						isInsert = pdao.insert(p);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					
+					if(isInsert>0)
+						System.out.println("Record Stored Successfully");
+					else 
+						System.out.println("Something Went Wrong");
 				}
 				break;
-				
-			case 4 :
+					
+				case 2 : {
+					System.out.print("Enter id : ");
+					int id = sc.nextInt();
+					Product p = null;
+					try {
+						p = pdao.search(id);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if(p==null) {
+						System.out.println("Prodcut not found");
+					}
+					else {
+						String ch;
+						System.out.print("Do you want to delete Y/N : ");
+						ch = sc.next();
+						if(ch.equalsIgnoreCase("y")) {
+							int isDelete = 0;
+							try {
+								isDelete = pdao.delete(id);
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+							if(isDelete==1) {
+								System.out.println("Prodcuted Deleted");
+							}
+							else {
+								System.out.println("Something went wrong");
+							}
+						}
+						else {
+							System.out.println("Operation Terminate");
+						}
+					}	
+				}
 				break;
-				
-			case 5 :
+					
+				case 3 : {
+					List<Product> pl = null;
+					try {
+						pl = pdao.display();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					
+					for(Product ps : pl) {
+						System.out.println(ps);
+					}
+				}
 				break;
-				
-			case 6 :
-				System.out.println("Thank You For Using Our Program");
-				break;
-				
-			default :
-				System.out.println("Invalid Choice");
-				break;
-		}
+					
+				case 4 : {
+					System.out.print("Enter id : ");
+					int id = sc.nextInt();
+					Product p = null;
+					try {
+						p = pdao.search(id);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					if(p==null) {
+						System.out.println("Proudct not found");
+					}
+					else {
+						System.out.println(p);
+					}
+					break;
+				}
+					
+				case 6 :
+					System.out.println("Thank You For Using Our Program");
+					break;
+					
+				default :
+					System.out.println("Invalid Choice");
+					break;
+			}
+		} while(choice != 6);
 		
 		sc.close();
 	}
